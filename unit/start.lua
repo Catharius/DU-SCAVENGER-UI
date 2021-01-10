57772d7de6a6d5d29e57cade11e48508665fa17f
Catharius <72415020+Catharius@users.noreply.github.com>
2021-01-11 00:08:32 +0100

    Update start.lua

--- unit/start.lua
+++ unit/start.lua
@@ -1,3 +1,14 @@
 -- Add this at the end of the unit start
 unit.setTimer("showzone",1)
+-- Init slots
+sortSlot(slot1)
+sortSlot(slot2)
+sortSlot(slot3)
+sortSlot(slot4)
+sortSlot(slot5)
+sortSlot(slot6)
+sortSlot(slot7)
+sortSlot(slot8)
+sortSlot(slot9)
+sortSlot(slot10)
 system.showScreen(1)
