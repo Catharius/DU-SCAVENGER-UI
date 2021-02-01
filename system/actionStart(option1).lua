648cf9dd97fb4bd9a40531cbd7d74fb20bc7f1b2
Catharius <72415020+Catharius@users.noreply.github.com>
2021-02-02 00:08:09 +0100

    Correcting maintain

--- system/actionStart(option1).lua
+++ system/actionStart(option1).lua
@@ -3,6 +3,7 @@ if stabilizeShip==true then
     stabilizeShip = false
 else
     stabilizeShip = true
+    TargetAltitude = core.getAltitude()--We take the current one
 end    
 -- restore yaw to zero
 yawInput = 0
